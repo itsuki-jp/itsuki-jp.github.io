4c6e6450ac99aec8fae288dcb7894c24d9eec280
itsuki-jp <itsuki19601@gmail.com>
2021-12-21 10:51:37 +0900

    知情演習と同期
    全体的に変更した

---
+++ navigation.js
@@ -0,0 +1,11 @@
+let header_js = document.getElementsByClassName("header_flex")[0];
+header_js.innerHTML = `
+<a href="index.html">
+<img id="logo" src="img/logo.png " alt="Logo "></a>
+<nav>
+    <ul class="main_nav ">
+        <li><a href="games.html ">Games</a></li>
+        <li><a href="profile.html ">Profile</a></li>
+    </ul>
+</nav>
+`;
